1fa60cf826a0de26857cce7e4cb0af9ac45b9ee9
Fran <framunnow8@cs.tru.ca>
2018-04-10 23:38:38 -0700

    added table sort and design updates

--- mainpage.php
+++ mainpage.php
@@ -80,7 +80,7 @@ include_once('functions.php');
 		<div class="container-fluid" id="tools-container">
                     <table class="table">
 			<thead><tr>
-			  <th style='text-align:left;border:none;'><label>Sort by:</label>&nbsp;<select class="selectpicker show-tick"><option>Closest distance</option></select></th>			       <th style='text-align:center;border:none;'><input type="text" placeholder="&nbsp;Search here" id="search-bar"></input></th>
+			  <th style='text-align:left;border:none;'><input type="text" placeholder="&nbsp;Search here" id="search-bar"></input></th>
 			  <th style='text-align:right;border:none;'><button class="glyphicon glyphicon-plus" title="Add chat room" id="add-room"></button></th>
 			</tr></thead>
 		    </table>
@@ -114,6 +114,7 @@ function addPosition(position) {
 </script>
 <script>
 $(document).ready(function() {
+
 <?php
 	if (isset($_SESSION['display_type'])) {
 		if ($_SESSION['display_type'] == 'set-radius') {
@@ -174,7 +175,49 @@ slider.oninput = function() {
 function get_distance(latitude1, longitude1, latitude2, longitude2) {
 	var ans = Math.sqrt((Math.pow((latitude2 - latitude1), 2) + Math.pow((longitude2 - longitude1), 2)));
 	return ans.toFixed(2);
-} 
+}
+
+function sortTable(table_id) {
+  var table, rows, switching, i, x, y, fx, fy, shouldSwitch;
+  table = document.getElementById(table_id);
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("tr");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 0; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/;
+      x = rows[i].getElementsByTagName("td")[1].innerHTML;
+      x = x.replace('<p id="distance-p"><i>', '');
+      x = x.replace('km away</i></p>', '');
+      fx = parseFloat(x);
+      y = rows[i + 1].getElementsByTagName("td")[1].innerHTML;
+      y = y.replace('<p id="distance-p"><i>', '');
+      y = y.replace('km away</i></p>', '');
+      fy = parseFloat(y);
+      //check if the two rows should switch place:
+      if (fx > fy) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+	console.log("switch true");
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 </script>
 <script>
 var xhttp = new XMLHttpRequest();
@@ -194,7 +237,6 @@ xhttp.onreadystatechange = function() {
 		if (data.length > 0) {
 			str = '<table class="table" id="rooms-table">';
                             for (var i = 0; i < data.length; i++) {
-                                str += '<tr>';
                                 for (var p in data[i]) {
                                    if (p == "date_created") r_date = data[i][p];
 				   if (p == "title") title = data[i][p];
@@ -203,7 +245,8 @@ xhttp.onreadystatechange = function() {
 				   if (p == "latitude") r_lat = data[i][p];
 				   if (p == "longitude") r_long = data[i][p];
 				}
-				if (get_distance(r_lat, r_long, u_lat, u_long) <= rad) { 
+				if (get_distance(r_lat, r_long, u_lat, u_long) <= rad) {
+					str += '<tr>'; 
 					str += '<td><div id="room-head">' + title + '</div><br><p id="p-date">' + r_date + '</p></td>';
 					str += '<td><p id="distance-p"><i>' + get_distance(r_lat, r_long, u_lat, u_long) + ' km away</i></p></td>';
 					str += '<td><button class="btn btn-primary">Join</button></td>';
@@ -217,6 +260,7 @@ xhttp.onreadystatechange = function() {
                             str += '</table>';
 		}
 		$('#rooms-list').html(str);
+		sortTable("rooms-table");
 	}
 };
 var url='//cs.tru.ca/~framunnow8/Project/final-proj/controller.php';
